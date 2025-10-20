feea47a4b1735ff9a1ff3026475fea6362e235ef
Mairus Maichrovicz Cabral <30597040+mairuswebber@users.noreply.github.com>
2025-10-20 20:24:33 +0000

    Adicionado o movimento do cavalo.

--- xadrez.c
+++ xadrez.c
@@ -2,16 +2,17 @@
 
 int main(){
 
-    char peca; //variavel para escolher qual peça mover
-    int torre = 1;
-    int bispo = 1;
-    int rainha = 1;
+    char peca; // variavel para a escolha de qual peça mover
+    int torre = 1; //variavel para repetição do movimento da torre
+    int bispo = 1; //variavel para repetição do movimento do bispo
+    int rainha = 1; //variavel para repetição do movimento da rainha
+    int cavaloV = 1; //variavel para repetição do movimento vertical do cavalo
+    int cavaloH = 1; //variavel para repetição do movimento horizontal do cavalo
 
-    printf("Desafio Jogo de Xadrez\n======================\n\n");
-    printf("Escolha qual peça você deseja mover. Torre [T], Bispo [B] ou Rainha [R]: ");
+    printf("Escolha qual peça você deseja mover. Torre [T], Bispo [B], Rainha [R] ou Cavalo [C]: ");
     scanf(" %c", &peca);
-    switch (peca) {
-    case 'T': // case para o movimento da torre, usando While.
+    switch (peca) { 
+    case 'T': // case para torre usando while
     case 't':
     printf("Movimento da torre:\n");
     while (torre <= 5) {
@@ -19,7 +20,7 @@ int main(){
         torre++;
     }
     break;
-    case 'B': // case para o movimento do bispo, usando Do-While.
+    case 'B':  // case para bispo usando do-while
     case 'b':
     printf("Movimento do bispo:\n");
     do {
@@ -27,9 +28,21 @@ int main(){
         bispo++;
     } while (bispo <= 5);
     break;
-    case 'R': // case para o movimento da rainha, usando For.
+    case 'R': //case para rainha usando for
     case 'r':
-    for (rainha; rainha <= 8; rainha++) {
+    printf("Movimento da rainha:\n");
+    for (rainha; rainha <= 8; rainha++) { 
+        printf("Esquerda\n");
+    }
+    break;
+    case 'C': //case para cavalo usando loops aninhados de for e while
+    case 'c':
+    printf("Movimento do cavalo:\n");
+    for (cavaloH; cavaloH <= 1; cavaloH++) {
+        while (cavaloV <= 2) {
+            printf("Baixo\n");
+            cavaloV++;
+        }
         printf("Esquerda\n");
     }
     break;
